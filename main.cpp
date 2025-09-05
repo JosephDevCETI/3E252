f0906a1803c02a89d35fa0ace5cf0fe96b757f7a
JosephDevCETI <a24310402@live.ceti.mx>
2025-09-05 03:17:54 +0000

    mi primer commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> //libreria 
+
+using namespace std; //abrebiacion 
+
+int main(int argc, char const *argv[]){ //main
+
+    string nombre;
+
+    cout << "Nombre: " ;
+    cin >> nombre;
+    cout << "Tu nombre es: " << nombre << endl;
+
+    return 0;
+}
